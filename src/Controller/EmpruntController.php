57e40e0009f219c371c7625418f7c8f2cb4854c2
jibundeyare <contact@jibundeyare.com>
2023-07-07 10:37:37 +0200

    Finalisation du CRUD des emprunts

--- src/Controller/EmpruntController.php
+++ src/Controller/EmpruntController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 #[Route('/emprunt')]
 class EmpruntController extends AbstractController
@@ -34,6 +35,8 @@ class EmpruntController extends AbstractController
     #[Route('/new', name: 'app_emprunt_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EmpruntRepository $empruntRepository): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
         $emprunt = new Emprunt();
         $form = $this->createForm(EmpruntType::class, $emprunt);
         $form->handleRequest($request);
@@ -53,6 +56,12 @@ class EmpruntController extends AbstractController
     #[Route('/{id}', name: 'app_emprunt_show', methods: ['GET'])]
     public function show(Emprunt $emprunt): Response
     {
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            if ($this->getUser()->getEmprunteur() != $emprunt->getEmprunteur()) {
+                throw new AccessDeniedException();
+            }
+        }
+
         return $this->render('emprunt/show.html.twig', [
             'emprunt' => $emprunt,
         ]);
@@ -61,6 +70,8 @@ class EmpruntController extends AbstractController
     #[Route('/{id}/edit', name: 'app_emprunt_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Emprunt $emprunt, EmpruntRepository $empruntRepository): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
         $form = $this->createForm(EmpruntType::class, $emprunt);
         $form->handleRequest($request);
 
@@ -79,6 +90,8 @@ class EmpruntController extends AbstractController
     #[Route('/{id}', name: 'app_emprunt_delete', methods: ['POST'])]
     public function delete(Request $request, Emprunt $emprunt, EmpruntRepository $empruntRepository): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
         if ($this->isCsrfTokenValid('delete'.$emprunt->getId(), $request->request->get('_token'))) {
             $empruntRepository->remove($emprunt, true);
         }
