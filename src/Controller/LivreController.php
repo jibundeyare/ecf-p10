c6f3cb10cac414a6e6359ed769005920f2723bd6
jibundeyare <contact@jibundeyare.com>
2023-07-07 11:57:32 +0200

    Création de CRUD des livres

---
+++ src/Controller/LivreController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Livre;
+use App\Form\LivreType;
+use App\Repository\LivreRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/livre')]
+class LivreController extends AbstractController
+{
+    #[Route('/{id}', name: 'app_livre_show', methods: ['GET'])]
+    public function show(Livre $livre): Response
+    {
+        return $this->render('livre/show.html.twig', [
+            'livre' => $livre,
+        ]);
+    }
+}
---
+++ src/Form/LivreType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Auteur;
+use App\Entity\Livre;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LivreType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('anneeEdition')
+            ->add('nombrePages')
+            ->add('codeIsbn')
+            ->add('auteur', EntityType::class, [
+                'class' => Auteur::class,
+                'choice_label' => function(Auteur $auteur) {
+                    return "{$auteur->getPrenom()} {$auteur->getNom()}";
+                },
+                'multiple' => false,
+                'expanded' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Livre::class,
+        ]);
+    }
+}
